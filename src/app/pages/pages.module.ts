c7ca7054a8d9c800cd6ef6ed729ab84b7fa6d021
kev3334 <kev3334@gmail.com>
2022-12-29 01:19:29 -0500

    Se agregan los routing para Home y se vincula con los componenetes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    HomeComponent
   ],
   imports: [
     CommonModule
   ],
-  exports:[
-    NavBarComponent
+  exports: [
+    HomeComponent
   ]
 })
-export class UsuariosModule { }
+export class PagesModule { }
